fd66152816aa1d45c54184ccd734e744ff007122
Lukas Hunziker <lukas.hunziker@students.fhnw.ch>
2015-12-28 18:21:20 +0100

    added some files

---
+++ src/main/java/ch/fhnw/oop2/swissmountainapp/MainMountainApp.java
@@ -0,0 +1,18 @@
+package ch.fhnw.oop2.swissmountainapp;/**
+ * Created by Lukas on 28.12.2015.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMountainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
